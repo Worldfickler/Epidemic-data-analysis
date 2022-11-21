1a326f6235fbf239b72801b7f7f43fe3be7b16b9
fickler <2586844575@qq.com>
2022-11-21 16:33:01 +0800

    Initial commit ---- 郭聚辉

---
+++ src/main/java/com/example/epidemicdataanalysis/EpidemicDataAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.example.epidemicdataanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpidemicDataAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpidemicDataAnalysisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/epidemicdataanalysis/EpidemicDataAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.epidemicdataanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpidemicDataAnalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
