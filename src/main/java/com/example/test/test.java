b4210f27575dd3dcca646c65c1184cb84e751fab
fickler <2586844575@qq.com>
2022-11-21 16:35:41 +0800

    test commit

---
+++ src/main/java/com/example/test/test.java
@@ -0,0 +1,16 @@
+package com.example.test;
+
+/**
+ * @author dell
+ * @version 1.0
+ */
+public class test {
+
+    public static void main(String[] args) {
+
+        System.out.println("项目初始化成功");
+        System.out.println("项目更新到github成功");
+
+    }
+
+}
