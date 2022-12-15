9e92fa9a451b6a2a98824865cf81946e0e08e1ec
zhumaoquan <1311214831@qq.com>
2022-12-15 17:30:42 +0800

    大洲

--- src/main/java/com/example/mapperReduce/continent_deadMR/ContinentDeadMapper.java
+++ src/main/java/com/example/mapperReduce/continent_deadMR/ContinentDeadMapper.java
@@ -2,6 +2,7 @@ package com.example.mapperReduce.continent_deadMR;
 
 import com.alibaba.fastjson2.JSON;
 import com.example.bean.CountryBean;
+import com.example.bean.CovidBean;
 import com.example.mapperReduce.continentMR.continentWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -16,7 +17,10 @@ public class ContinentDeadMapper extends Mapper<LongWritable, Text, Text, Contin
 
     @Override
     protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text,ContinentDeadWritable>.Context context) throws IOException, InterruptedException {
-        countryBean = JSON.parseObject(String.valueOf(value), CountryBean.class);
+
+        String line = new String(value.getBytes(),0,value.getLength(),"GBK");
+//        System.out.println("转换后的格式" + line);
+        countryBean = JSON.parseObject(String.valueOf(line), CountryBean.class);
 
 
         text.set(countryBean.getContinents());
