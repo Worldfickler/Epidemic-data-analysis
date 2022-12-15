ab459b698da35c21f48edcea7e001945d0329467
fickler <2586844575@qq.com>
2022-12-15 09:45:42 +0800

    fickler

--- src/main/java/com/example/mapperReduce/deadMR/DeadDriver.java
+++ src/main/java/com/example/mapperReduce/deadMR/DeadDriver.java
@@ -1,8 +1,54 @@
 package com.example.mapperReduce.deadMR;
 
+import com.example.mapperReduce.curedMR.CuredDriver;
+import com.example.mapperReduce.curedMR.CuredMapper;
+import com.example.mapperReduce.curedMR.CuredReducer;
+import com.example.mapperReduce.curedMR.CuredWritable;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
 /**
  * @author dell
  * @version 1.0
  */
 public class DeadDriver {
+
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+
+        //获取job
+        Configuration configuration = new Configuration();
+        Job job = Job.getInstance(configuration);
+        //设置jar包路径
+        job.setJarByClass(DeadDriver.class);
+        //关联mapper和reducer
+        job.setMapperClass(DeadMapper.class);
+        job.setReducerClass(DeadReducer.class);
+        //设置map输出的key，value输出类型
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(DeadWritable.class);
+        //设置最终输出的key，value类型
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(DeadWritable.class);
+        //设置输入路径和输出路径
+        FileInputFormat.setInputPaths(job, new Path("data/province_epidemic.txt"));
+        FileOutputFormat.setOutputPath(job, new Path("data/dead_output"));
+
+        FileSystem fs = FileSystem.get(configuration);
+        if(fs.exists(new Path("data/province_epidemic.txt"))){
+            fs.delete(new Path("data/dead_output"),true);
+        }
+
+        //提交job
+        boolean result = job.waitForCompletion(true);
+        System.exit(result ? 0 : 1);
+
+    }
+
 }
--- src/main/java/com/example/mapperReduce/suspectedMR/SuspectedReducer.java
+++ src/main/java/com/example/mapperReduce/suspectedMR/SuspectedReducer.java
@@ -1,8 +1,26 @@
 package com.example.mapperReduce.suspectedMR;
 
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
 /**
  * @author dell
  * @version 1.0
  */
-public class SuspectedReducer {
+public class SuspectedReducer extends Reducer<Text, SuspectedWritable, Text, SuspectedWritable> {
+
+    SuspectedWritable suspectedWritable = new SuspectedWritable();
+
+    @Override
+    protected void reduce(Text key, Iterable<SuspectedWritable> values, Reducer<Text, SuspectedWritable, Text, SuspectedWritable>.Context context) throws IOException, InterruptedException {
+        int suspectedCount = 0;
+        for (SuspectedWritable value : values) {
+            suspectedCount += value.getSuspectedCount();
+        }
+        suspectedWritable.setProvinceShortName(String.valueOf(key));
+        suspectedWritable.setSuspectedCount(suspectedCount);
+        context.write(null, suspectedWritable);
+    }
 }
